58a204034d625825dd90a59d4c1b0cf3d491a991
eric3119 <esc2@ic.ufal.br>
2019-08-01 09:51:14 -0300

    update

--- projeto-ab1/src/Category.hpp
+++ projeto-ab1/src/Category.hpp
@@ -1,12 +1,18 @@
 #ifndef PROJETO_AB1_CATEGORY_HPP
 #define PROJETO_AB1_CATEGORY_HPP
 
-enum class Category { Init=1, Var, PtVg,
-    Pt2, Vg, Integer, Float, Char, Boolean, Procedure,
-    AbPar, FePar, Read, Write, For, To, Do, Repeat, Until,
-    If, Then, Else, Eq, Maior, Menor, MaiorEq, MenorEq, Difer,
-    Mais, Menos, Mult, Div, Id, CteI, CteR,
-    Eof
+const int MAX_CAT_LEN = 10;
+
+enum class Category {
+    Init=1, Integer, Float, Char, Boolean,
+    PtVg, Pt2, Vg, AbPar, FePar, AbCol, FeCol, AbChav, FeChav,
+    Procedure, Return, Break,
+    Read, Write,
+    For, While, If, ElseIf, Else,
+    OpEq, OpMaior, OpMenor, OpMaiorEq, OpMenorEq, OpDifer,
+    OpMais, OpMenos, OpMult, OpDiv, OpMod,
+    OpAtr,
+    Id, CteInt, CteFloat, CteChar, CteBool, CteStr, Eof
 };
 
 char* categ_name(Category anEnum);
