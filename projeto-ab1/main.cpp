71e56092ce687804b10239ba1ffe1ff5b9166d6e
eric3119 <eric.s.coelho@gmail.com>
2019-08-01 13:35:17 -0300

    update LF

--- projeto-ab1/main.cpp
+++ projeto-ab1/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, const char **argv){
 
     if (argc != 2){
 
-        cout << "use: main prog.lang" << endl;
+        cout << "use: main prog.ec" << endl;
 
         exit(0);
     }
