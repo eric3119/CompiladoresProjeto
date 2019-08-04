8879ea974f828d7908a2fb5a0ff463a793bdc8f4
eric3119 <esc2@ic.ufal.br>
2019-08-04 15:58:02 -0300

    init interpretador calculadora

---
+++ interpretador-calculadora/calc.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char* argv[]) {
+
+    if(argc != 2){
+        cout << "use: calc expr=\n";
+        exit(0);
+    }
+
+    return 0;
+}
