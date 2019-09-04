1ee1f164d1c54f360f7ddf826ca640eccec763e3
eric3119 <esc2@ic.ufal.br>
2019-09-04 10:39:39 -0300

    init parser

--- projeto-ab2/src/Parser.cpp
+++ projeto-ab2/src/Parser.cpp
@@ -3,17 +3,191 @@
 using namespace std;
 
 void Parser::parse() {
-
-    queue<string> Q;
-
-    Q.push("Inicio");
-
-    while(!Q.empty()){
-        string s = Q.front();
-        Q.pop();
-    }
-
+    tk = tokenizer.nextToken();
+    int res = inicio();
+    cout << "\nParse " << (res == OK? "OK":"ERROR") << endl;
 }
 
 Parser::Parser(const std::string& filename) : tokenizer(filename) {}
+
+int Parser::inicio(){
+
+    int res = OK;
+
+    if(
+        tk.categ == Category::Integer ||
+        tk.categ == Category::Float ||
+        tk.categ == Category::Char ||
+        tk.categ == Category::Boolean ||
+        tk.categ == Category::String ||
+        tk.categ == Category::Procedure){
+        cout << "          Início = Tipo ‘id’ InícioF\n";
+        res = tipo();
+        if(res == ERRO)
+            return res;
+
+        if (tk.categ == Category::Id){
+            nextToken();
+            res = inicioF();
+            if(res == ERRO)
+                return res;
+        } else
+            return ERRO;
+    }else if (tk.categ == Category::Const){
+        cout << "          Início = ‘const’ Tipo ‘id’ ListaAtr ‘;’ Início\n";
+        nextToken();
+        res = tipo();
+        if(res == ERRO)
+            return res;
+
+        res = listaAtr();
+        if(res == ERRO)
+            return res;
+
+        if(tk.categ == Category::PtVg){
+            nextToken();
+            res = inicio();
+            if(res == ERRO)
+                return res;
+        }
+    }else if(tk.categ == Category::Eof)
+        cout << "          Início = epsilon\n";
+    return res;
+}
+int Parser::inicioF(){
+    int res = OK;
+    if(tk.categ == Category::AbPar){
+        cout << "          InícioF = Função Início\n";
+        res = funcao();
+    }else if(
+            tk.categ == Category::AbCol||
+            tk.categ == Category::Vg||
+            tk.categ == Category::PtVg||
+            tk.categ == Category::OpAtr
+            ) {
+        cout << "          InícioF = Variável Início\n";
+        res = variavel();
+    }
+    if (res == ERRO) return res;
+
+    return inicio();
+}
+int Parser::bloco(){
+
+}
+int Parser::blocoF(){}
+int Parser::funcao(){
+    int res = OK;
+
+    if (tk.categ == Category::AbPar){
+        nextToken();
+
+        res = listaParam();
+        if (tk.categ == Category::FePar){
+            nextToken();
+            res = funcaoF();
+        } else return ERRO;
+    }else return ERRO;
+
+    return res;
+}
+int Parser::funcaoF(){
+    int res = OK;
+
+    if (tk.categ == Category::AbChav){
+        nextToken();
+
+        res = bloco();
+        if (tk.categ == Category::FeChav){
+            nextToken();
+            if (tk.categ == Category::PtVg){
+                nextToken();
+            }
+        } else return ERRO;
+    }else if (tk.categ == Category::PtVg){
+        nextToken();
+    }else return ERRO;
+
+    return res;
+}
+int Parser::variavel(){}
+int Parser::listaId(){}
+int Parser::listaIdR(){}
+int Parser::listaAtr(){}
+int Parser::listaAtrR(){}
+int Parser::array(){}
+int Parser::arrayF(){}
+int Parser::sentencas(){}
+int Parser::comando(){}
+int Parser::entrada(){}
+int Parser::saida(){}
+int Parser::fIf(){}
+int Parser::fElseIf(){}
+int Parser::fElse(){}
+int Parser::fWhile(){}
+int Parser::fFor(){}
+int Parser::desvio(){}
+int Parser::atribuicao(){}
+int Parser::listaParam(){}
+int Parser::listaParamR(){}
+int Parser::listaArgs(){}
+int Parser::listaArgsR(){}
+int Parser::tipo(){
+    int res = OK;
+
+    /*switch(tk.categ){
+        case Category::Integer:
+        case Category::Float:
+        case Category::Char:
+        case Category::Boolean:
+        case Category::String:
+        case Category::Procedure:
+        default:
+    }*/
+}
+int Parser::expressao(){}
+int Parser::exprAritm(){}
+int Parser::exprAritR(){}
+int Parser::exprBool(){}
+int Parser::exprBoolR(){}
+int Parser::exprConcat(){}
+int Parser::exprConcatR(){}
+int Parser::termoAritm(){}
+int Parser::termoAritmR(){}
+int Parser::termoBool(){}
+int Parser::termoBoolR(){}
+int Parser::termoConcat(){}
+int Parser::termoConcatR(){}
+int Parser::fatorAritm(){}
+int Parser::fatorAritmF(){}
+int Parser::fatorBool(){}
+int Parser::fatorConcat(){}
+int Parser::listaArray(){}
+int Parser::listaArrayR(){}
+int Parser::unario(){}
+int Parser::relac(){}
+int Parser::igual(){}
+int Parser::logico(){}
+int Parser::adit(){}
+int Parser::mult(){}
+int Parser::opEq(){}
+int Parser::opMaior(){}
+int Parser::opMenor(){}
+int Parser::opMaiorEq(){}
+int Parser::opMenorEq(){}
+int Parser::opDifer(){}
+int Parser::opAnd(){}
+int Parser::opOr(){}
+int Parser::opNot(){}
+int Parser::opMais(){}
+int Parser::opMenos(){}
+int Parser::opMult(){}
+int Parser::opDiv(){}
+int Parser::opMod(){}
+int Parser::opAtr(){}
+int Parser::opConcat(){}
+
+void Parser::nextToken() {
+    tk = tokenizer.nextToken();
+}
 
--- projeto-ab2/src/Parser.h
+++ projeto-ab2/src/Parser.h
@@ -6,9 +6,13 @@
 #include <string>
 #include "../../projeto-ab1/src/Tokenizer.h"
 
+#define ERRO 0
+#define OK 1
+
 class Parser {
 private:
     Token tk{};
+    void nextToken();
 
     std::ifstream source;
 public:
@@ -17,6 +21,77 @@ public:
     void parse();
 
     Parser(const std::string& filename);
+
+    int inicio();
+    int inicioF();
+    int bloco();
+    int blocoF();
+    int funcao();
+    int funcaoF();
+    int variavel();
+    int listaId();
+    int listaIdR();
+    int listaAtr();
+    int listaAtrR();
+    int array();
+    int arrayF();
+    int sentencas();
+    int comando();
+    int entrada();
+    int saida();
+    int fIf();
+    int fElseIf();
+    int fElse();
+    int fWhile();
+    int fFor();
+    int desvio();
+    int atribuicao();
+    int listaParam();
+    int listaParamR();
+    int listaArgs();
+    int listaArgsR();
+    int tipo();
+    int expressao();
+    int exprAritm();
+    int exprAritR();
+    int exprBool();
+    int exprBoolR();
+    int exprConcat();
+    int exprConcatR();
+    int termoAritm();
+    int termoAritmR();
+    int termoBool();
+    int termoBoolR();
+    int termoConcat();
+    int termoConcatR();
+    int fatorAritm();
+    int fatorAritmF();
+    int fatorBool();
+    int fatorConcat();
+    int listaArray();
+    int listaArrayR();
+    int unario();
+    int relac();
+    int igual();
+    int logico();
+    int adit();
+    int mult();
+    int opEq();
+    int opMaior();
+    int opMenor();
+    int opMaiorEq();
+    int opMenorEq();
+    int opDifer();
+    int opAnd();
+    int opOr();
+    int opNot();
+    int opMais();
+    int opMenos();
+    int opMult();
+    int opDiv();
+    int opMod();
+    int opAtr();
+    int opConcat();
 
 };
 
